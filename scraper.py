59895f1749270447025aa8915f02f4eee8b2720b
Gnorbi951 <gnorbi951@gmail.com>
2020-03-09 21:13:03 +0100

    elements now can be found

--- scraper.py
+++ scraper.py
@@ -4,7 +4,15 @@ from bs4 import BeautifulSoup as soup
 
 url = "https://www.arukereso.hu/videokartya-c3142/"
 
+# Read the whole page
 uClient = uReq(url)
 page_response = uClient.read()
-print(page_response)
 uClient.close()
+
+# Parse html
+page = soup(page_response, "html.parser")
+
+# Find the divs I'm looking for
+elements = page.findAll("div", {"class": "product-box-container"})
+
+print(len(elements))
